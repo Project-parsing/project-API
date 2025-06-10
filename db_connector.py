a8ed19cf949a770294fad081dd7be874bfa0fb92
zmnxc1 <zmnxc1@gmail.com>
2025-06-10 15:54:41 +0300

    Update db_connector.py

--- db_connector.py
+++ db_connector.py
@@ -8,27 +8,30 @@ class DatabaseConnector:
         self.cursor = self.con.cursor()
         self.hasher = ArgonHasher()
 
-    def insert_user(self, name, surname, patronymic, login, password, filepath):
+    def insert_user(self, name, surname,  login, password):
         hashed_password = self.hasher.hash(password)
-        information = (name, surname, patronymic, login, hashed_password, filepath)
-        self.cursor.execute("INSERT INTO tgParsing (name, surname, patronymic, login, hash_password) VALUES (?,?,?,?,?,?)", information )
+        information = (name, surname,  login, hashed_password)
+        self.cursor.execute("INSERT INTO webUsers (name, surname, login, hash_password) VALUES (?,?,?,?)", information )
         self.con.commit()
         return True
 
     def login(self, tryLogin, tryPassword):
-        self.cursor.execute("SELECT hash_password FROM webUsers WHERE  login = ? ", (tryLogin))
-        res = self.cursor.fetchall()
-        if len(res) > 0:
-            return self.hasher.verify(self, res[0], tryPassword)
+        self.cursor.execute("SELECT `hash_password` FROM `webUsers` WHERE `login` = ?", (tryLogin,))
+        res = self.cursor.fetchall() 
+        if len(res) == 1:
+            return self.hasher.verify(res[0][0], tryPassword)
         else:
             return False
+        
 
-    def insert_tg_post(self):
-        return False
+    def insert_tg_post(self, sourse, date, header, keyWords,message, author):
+        information = (sourse, date, header, keyWords, message, author)
+        self.cursor.execute("INSERT INTO tgParsing (sourse, date, header, keyWords, message, author) VALUES (?,?,?,?,?,?)", information )
+        self.con.commit()
+        return True
 
-    def insert_web_post(self):
-        return False
-
-def main():
-    db_executor = DatabaseClient("db.db")
-    db_executor.insert_tg_post()
+    def insert_web_post(self, sourse, date, header, keyWords,message, author):
+        information = (sourse, date, header, keyWords, message, author)
+        self.cursor.execute("INSERT INTO tgParsing (sourse, date, header, keyWords, message, author) VALUES (?,?,?,?,?,?)", information )
+        self.con.commit()
+        return True
